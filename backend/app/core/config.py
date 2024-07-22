523e2d1b7eeeff39b821ed5e0458a613289b0794
vitaliy.pavliy <vitaliy.pavliy@raftds.com>
2024-07-23 01:28:37 +0300

    add token_secrets

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -33,6 +33,8 @@ class PostgresConfig(BaseModel):
 
 class AccessToken(BaseModel):
     lifetime_seconds: int = 3600
+    reset_password_token_secret: str
+    verification_token_secret: str
 
 
 class Settings(BaseSettings):
