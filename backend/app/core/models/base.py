4e152f694dfffce05427ccab9e453a91e21127ba
vitaliy.pavliy <vitaliy.pavliy@raftds.com>
2024-07-22 19:07:36 +0300

    add naming_convention for MetaData

--- backend/app/core/models/base.py
+++ backend/app/core/models/base.py
@@ -4,11 +4,16 @@ from sqlalchemy.orm import declared_attr
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import Mapped
 
+from core.config import settings
 from utils import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
