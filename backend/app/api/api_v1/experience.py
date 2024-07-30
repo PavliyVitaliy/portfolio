eb173439344dd5d2a2699e37dc2ae62a99ad937f
vitaliy.pavliy <vitaliy.pavliy@raftds.com>
2024-07-30 16:37:47 +0300

    create views for authenticated users and superusers

--- backend/app/api/api_v1/experience.py
+++ backend/app/api/api_v1/experience.py
@@ -1,8 +1,42 @@
-from fastapi import APIRouter
+from typing import Annotated
 
+from fastapi import APIRouter, Depends
+
+from api.api_v1.fastapi_users import (
+    current_active_user,
+    current_active_superuser,
+)
 from core.config import settings
+from core.models import User
+from core.schemas.user import UserRead
 
 router = APIRouter(
     prefix=settings.api.v1.experience,
     tags=["Experience"],
 )
+
+
+@router.get("")
+def get_user_experience(
+        user: Annotated[
+            User,
+            Depends(current_active_user),
+        ],
+):
+    return {
+        "user": UserRead.model_validate(user),
+        "experience": ["exp1", "exp2", "exp3"],
+    }
+
+
+@router.get("/secrets")
+def get_superuser_experience(
+    user: Annotated[
+        User,
+        Depends(current_active_superuser),
+    ],
+):
+    return {
+        "user": UserRead.model_validate(user),
+        "experience": ["secret-exp1", "secret-exp2", "secret-exp3"],
+    }
