ea61e0d0a3983ea01fa1157d8f9c5eeabae30bb0
vitaliy.pavliy <vitaliy.pavliy@raftds.com>
2024-08-07 00:33:38 +0300

    add get_free_experience router

--- backend/app/api/api_v1/experience.py
+++ backend/app/api/api_v1/experience.py
@@ -1,14 +1,17 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.api_v1.fastapi_users import (
     current_active_user,
     current_active_superuser,
 )
 from core.config import settings
-from core.models import User
+from core.models import User, db_helper
 from core.schemas.user import UserRead
+from services.users import get_super_user
+from services.experience import get_experience
 
 router = APIRouter(
     prefix=settings.api.v1.experience,
@@ -40,3 +43,15 @@ def get_superuser_experience(
         "user": UserRead.model_validate(user),
         "experience": ["secret-exp1", "secret-exp2", "secret-exp3"],
     }
+
+
+@router.get("/free")
+async def get_free_experience(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter)
+    ],
+):
+    super_user = await get_super_user(session=session)
+    experience = await get_experience(str(super_user.id))
+    return experience
